9d9bace43c0548dc5e5ffc6c37fb5e8ccd48f884
Tyom Semonov <inspiro@gmail.com>
2019-05-18 13:34:27 +0100

    Improve DDG answers feature

--- src/features/ddg-answer.js
+++ src/features/ddg-answer.js
@@ -1,8 +1,13 @@
 const { duckIt } = require('node-duckduckgo');
 const { isString } = require('lodash');
-const { block, object, TEXT_FORMAT_MRKDWN } = require('slack-block-kit');
+const {
+  block,
+  element,
+  object,
+  TEXT_FORMAT_MRKDWN,
+} = require('slack-block-kit');
 const { text } = object;
-const { section, context, image } = block;
+const { section, divider, context, image } = block;
 
 module.exports = controller => {
   const re = /^(describe|ddg) (.*)$/i;
@@ -14,31 +19,52 @@ module.exports = controller => {
       const [, , query] = message.text.match(re);
 
       try {
-        const { data } = await duckIt(query, { parentalFilter: 'Deactivated' });
+        const { data } = await duckIt(query, {
+          parentalFilter: 'Deactivated',
+        });
         const {
           Heading,
           Abstract,
           AbstractURL,
           Image,
-          Infobox = { content: [] },
+          Infobox,
           Results = [],
           RelatedTopics = [],
         } = data;
 
         if (!Heading || !Abstract) {
           if (RelatedTopics.length) {
-            return bot.say(
-              `*${query}* is too ambiguous. Try to narrow it down.`,
-            );
+            const examples = RelatedTopics.filter(({ FirstURL }) => FirstURL)
+              .map(({ FirstURL }) =>
+                FirstURL.replace(/^.*\/(\w+).*/, '$1').replace(/_/g, ' '),
+              )
+              .filter(e => e.toLowerCase() !== query.toLowerCase())
+              .map(e => `*${e}*`)
+              .join(', ');
+            return bot.say({
+              blocks: [
+                section(
+                  text(
+                    `*${query}* is too ambiguous. Try to narrow it down.`,
+                    TEXT_FORMAT_MRKDWN,
+                  ),
+                ),
+                context([text(`Examples: ${examples}...`, TEXT_FORMAT_MRKDWN)]),
+              ],
+            });
           }
           return bot.say(`I’ve nothing on *${query}*.`);
         }
         const resultsData = Results.map(({ FirstURL, Text }) =>
           text(`<${FirstURL}|${Text}>`, TEXT_FORMAT_MRKDWN),
-        );
-        const infoboxData = Infobox.content
-          .filter(c => isString(c.label) && isString(c.value))
-          .map(c => text(`${c.label}: *${c.value}*`, TEXT_FORMAT_MRKDWN));
+        ).slice(0, 10);
+        const infoboxData =
+          Infobox && Infobox.content && Infobox.content.length
+            ? Infobox.content
+                .filter(c => isString(c.label) && isString(c.value))
+                .map(c => text(`${c.label}: *${c.value}*`, TEXT_FORMAT_MRKDWN))
+                .slice(0, 10)
+            : [];
 
         const heading =
           Heading && AbstractURL
@@ -46,20 +72,26 @@ module.exports = controller => {
             : `*${Heading}*\n` || '';
         const abstract = Abstract || '';
 
-        await bot.say({
-          blocks: [
-            section(
-              text(`${heading}${abstract}`, TEXT_FORMAT_MRKDWN),
-              Image
-                ? {
-                    accessory: image(Image, Heading),
-                  }
-                : undefined,
-            ),
-            resultsData.length && context(resultsData),
-            infoboxData.length && context(infoboxData),
-          ].filter(Boolean),
-        });
+        const blocks = [
+          section(
+            text(`${heading}${abstract}`, TEXT_FORMAT_MRKDWN),
+            Image
+              ? {
+                  accessory: image(Image, Heading),
+                }
+              : undefined,
+          ),
+        ];
+
+        if (resultsData.length) {
+          blocks.push(context(resultsData));
+        }
+
+        if (infoboxData.length) {
+          blocks.push(divider(), context(infoboxData));
+        }
+
+        await bot.say({ blocks });
       } catch (error) {
         console.error(error);
       }
